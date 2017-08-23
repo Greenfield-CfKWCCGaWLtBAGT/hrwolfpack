44ff70e2346da67ad86a6adc4451e4032b4b85c6
tyler carson <tyler.c.carson@gmail.com>
2017-08-22 17:14:22 -0700

    delete config file

--- config.example.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  associate_tag: '', // Your AWS Associate Tag
-  access_key_id: '', // Your AWS_ACCESS_KEY
-  secret_key: '', // Your AWS_SECRET_KEY
-  db_name: '',
-  db_username: '', // Your db username, eg. root
-  db_password: '',
-  db_host: '' // eg. localhost
-};
