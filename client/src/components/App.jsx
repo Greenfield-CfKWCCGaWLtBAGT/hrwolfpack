778ef842112e8755bd30caa06cd47255524991d6
Karun Pavithran <karunpavithran6892@gmail.com>
2017-08-24 10:11:41 -0700

    fix listing page

---
+++ client/src/components/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header.jsx';
+import Main from './Main.jsx';
+import io from 'socket.io-client';
+
+
+let env = window.location.hostname + ':' + window.location.port;
+let socket = io(env);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: '',
+      currentUser: ''
+    };
+  }
+
+  componentDidMount() {
+    $.get('/user', (data) => {
+      this.setState({
+        userId: data.id,
+        currentUser: data.username
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Main userId={this.state.userId} socket={socket}/>
+      </div>
+    );
+  }
+}
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
+import App from './components/App.jsx';
 import io from 'socket.io-client';
-
-let env = window.location.hostname + ':' + window.location.port;
-let socket = io(env);
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userId: '',
-      currentUser: ''
-    };
-  }
-
-  componentDidMount() {
-    $.get('/user', (data) => {
-      this.setState({
-        userId: data.id,
-        currentUser: data.username
-      });
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser}/>
-        <Main userId={this.state.userId} socket={socket}/>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render((
   <BrowserRouter>
@@ -43,4 +13,3 @@ ReactDOM.render((
   </BrowserRouter>
 ), document.getElementById('app'));
 
-export default App;
