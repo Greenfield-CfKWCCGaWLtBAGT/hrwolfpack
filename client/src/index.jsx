0c07f0df091281d2c36b1df998193c90961dd6ba
tyler carson <tyler.c.carson@gmail.com>
2017-08-23 21:55:58 -0700

    router changes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
 import App from './components/App.jsx';
