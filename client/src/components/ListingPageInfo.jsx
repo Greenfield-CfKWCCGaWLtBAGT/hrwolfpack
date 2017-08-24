f06aa2a1d9365e96cb2521b69189966b2ea06ecc
Karun Pavithran <karunpavithran6892@gmail.com>
2017-08-24 16:31:01 -0700

    listingpage works, create separate listing info component

---
+++ client/src/components/ListingPageInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import { ListGroup, Button, Modal, Col, Thumbnail, Grid, Row, Panel } from 'react-bootstrap';
+import { Link } from 'react-router-dom'; 
+
+export default class ListingPageInfo extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+}
