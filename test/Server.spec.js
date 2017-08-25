5094b4e4db01d81c37a31f64c3b709281571c681
Karun Pavithran <karunpavithran6892@gmail.com>
2017-08-24 17:12:33 -0700

    comment out db

--- test/Server.spec.js
+++ test/Server.spec.js
@@ -1,18 +1,18 @@
-var expect = require('chai').expect;
-var request = require('request');
-var db = require('../db');
-var chai = require('chai');
-var chaiHTTP = require('chai-http');
-var should = chai.should();
-var server = require('../server/index.js');
+// var expect = require('chai').expect;
+// var request = require('request');
+// // var db = require('../db');
+// var chai = require('chai');
+// var chaiHTTP = require('chai-http');
+// var should = chai.should();
+// var server = require('../server/index.js');
 
-chai.use(chaiHTTP);
+// chai.use(chaiHTTP);
 
-// describe('server', () => {
-//   describe('GET /', () => {
-//     it('should get the index.html', (done) => {
+// // describe('server', () => {
+// //   describe('GET /', () => {
+// //     it('should get the index.html', (done) => {
       
-//     })
+// //     })
 //   })
 // })
 
