e85aaee25389bc4e5d3d3eac1e85e648288cb7f1
Karun Pavithran <karunpavithran6892@gmail.com>
2017-08-25 16:20:28 -0700

    add package.json

--- test/Server.spec.js
+++ test/Server.spec.js
@@ -1,78 +1,96 @@
-// var expect = require('chai').expect;
-// var request = require('request');
-// // var db = require('../db');
-// var chai = require('chai');
-// var chaiHTTP = require('chai-http');
-// var should = chai.should();
-// var server = require('../server/index.js');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let app = require('../server/index.js');
 
-// chai.use(chaiHTTP);
+let request = require('supertest');
+let server = request.agent('http://localhost:3000');
 
-// describe('Create a new listing', function() {
+let expect = chai.expect;
 
-// })
+chai.use(chaiHttp);
 
+describe('Server Index', () => {
+  describe('Login Page', () => {
+    it('should respond with a server code 200', (done) => {
+      chai.request(app)
+        .get('/login')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it ('should serve static HTML', (done) => {
+      chai.request(app)
+        .get('/login')
+        .end((err, res) => {
+          expect(res).to.be.html;
+          done();
+        });
+    });
+    it ('should redirect to /login on bad input', (done) => {
+      server.post('/login')
+        .send(  {username: 'fake', password: 'news'}  )
+        .expect(302)
+        .expect('Location', '/')
+        .end((err, res) => {
+          done();
+        })
+    });
+    it ('should redirect to index on successful login', (done) => {
+      server.post('/login')
+        .send(  {username: 'karun', password: '123k'}  )
+        .expect(302)
+        .expect('Location', '/')
+        .end((err, res) => {
+          done();
+        })
+    });
 
-// describe('Server Side Testing', function() {
-//
-//   describe('Integration Testing', function() {
-//
-//     beforeEach(function(done) {
-//     	request({
-//     		method: 'POST',
-//     		uri: 'http://127.0.0.1:3000/login',
-//     		json: {
-//     			username: 'dylan',
-//     			password: '123d'
-//     		}
-//     	}, function(data) {
-//     		console.log(data);
-//     		done();
-//     	});
-//     });
-//
-//     // afterEach(function() {
-//
-//     // });
-//
-//     it('should insert into Listing after post request to "/listings"', function(done) {
-//       request({
-// 	      method: 'POST',
-// 	      uri: 'http://127.0.0.1:3000/listings',
-// 	      json: {
-// 		      name: '6 Packs of Chicken Breast',
-// 		      price: 12,
-// 		      location: '50 Webster, San Francisco, CA 94102'
-// 	  	  },
-//       }, function() {
-//       	db.Listing.findAndCountAll()
-//       	.then(result => {
-//       		expect(result.count).to.equal(4);
-//       	 	done();
-//       	});
-//       });
-//     });
-//
-//     it('should return index when the value is present', function() {
-//       expect([1,2,3].indexOf(3)).to.equal(2);
-//     });
-//
-//   });
-//
-// });
-
-
-// tests
-
-// after create listing is hit, new listing should be
-// inserted into the listing table
-
-// after good are here button is list, listing property "arrived"
-// in listing table should change to true
-
-// after a user clicks join listing, a new record on the userListing
-// table should be created with user_id and listing_id
-
-// after goods are received buttion is hit, the corressponding
-// record on the userListing table should be updated with property
-// received to true
+  });
+  describe('Signup Page', () => {
+    it('should respond with a server code 200', (done) => {
+      chai.request(app)
+        .get('/signup')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it ('should serve static HTML', (done) => {
+      chai.request(app)
+        .get('/signup')
+        .end((err, res) => {
+          expect(res).to.be.html;
+          done();
+        });
+    });
+    it ('should redirect to index on successful signup', (done) => {
+      server.post('/login')
+        .send(  {username: 'new', password: 'user'}  )
+        .expect(302)
+        .expect('Location', '/')
+        .end((err, res) => {
+          done();
+        })
+    });
+  });
+  describe('Logout Page', () => {
+    it('should respond with a server code 200', (done) => {
+      chai.request(app)
+        .get('/logout')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it ('should serve static HTML', (done) => {
+      chai.request(app)
+        .get('/logout')
+        .end((err, res) => {
+          expect(res).to.be.html;
+          done();
+        });
+    });
+  });
+  
+})
