4e64e52f91b646639408ad72f009f53a766e5f49
Ting Wang <twang1225@gmail.com>
2017-08-25 12:14:23 -0700

    connect socket between client and server for chatroom

--- client/src/components/Chatroom.jsx
+++ client/src/components/Chatroom.jsx
@@ -1,3 +1,5 @@
+require('.../dist/styles/styles.css');
+
 import React from 'react';
 import io from 'socket.io-client';
 
@@ -5,7 +7,7 @@ export default class Chatroom extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      messages: []
     }
   }
 
@@ -14,16 +16,35 @@ export default class Chatroom extends React.Component {
   }
 
   makeClientSocketConnection() {
+    //connect to server
     let socket = io.connect('http://localhost:3000');
-    socket.on('news', function (data) {
-    console.log(data);
+    //listen for messages from server
+    socket.on('server:message', (message) => {
+    console.log(message);
     socket.emit('my other event', { my: 'data' });
   });
   }
 
+  handleSend(message) {
+    const message = {
+      user: ,
+      message
+    }
+    //emit message to server
+    socket.emit('client:message', messageObject);
+  }
+
+  saveMessage() {
+
+  }
+
   render() {
     return (
-      <div>HELLO TING</div>
+        <div className="container">
+        <h3>Ferret Business Chat</h3>
+        <Messages messages={this.state.messages} />
+        <ChatInput onSend={this.sendHandler} />
+      </div>
     )
 
   }
