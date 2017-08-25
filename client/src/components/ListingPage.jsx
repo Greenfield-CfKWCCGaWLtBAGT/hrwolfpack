1d007ca04f45fbda1d44a3e34077897694290d05
Ting Wang <twang1225@gmail.com>
2017-08-25 11:12:24 -0700

    add chatroom component

--- client/src/components/ListingPage.jsx
+++ client/src/components/ListingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ListGroup, Button, Modal, Col, Thumbnail, Grid, Row, Panel } from 'react-bootstrap';
 import $ from 'jquery';
 import Listing from './Listing.jsx';
+import Chatroom from './Chatroom.jsx';
 
 var divStyle = {
   margin: '100px 50px 50px 50px'
@@ -26,10 +27,11 @@ export default class ListingPage extends React.Component {
       url: '/listing',
       success: (result) => {
         context.setState({
-          listing: result
+          listing: result[0]
         })
       }
     });
+    console.log('USERID:', this.props.userId, this.props.socket, this.props.history)
   }
 
   render() {
@@ -47,6 +49,9 @@ export default class ListingPage extends React.Component {
               socket={this.props.socket}
               history={this.props.history}
             />
+          </Row>
+          <Row>
+            <Chatroom />
           </Row>
         </Grid>
       )
