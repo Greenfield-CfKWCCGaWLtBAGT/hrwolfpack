e2ee1fa403da7dc0889c795e6b441eba9347a0fe
Ting Wang <twang1225@gmail.com>
2017-08-26 12:27:49 -0700

    add chatroom component to listing modal

--- client/src/components/Listing.jsx
+++ client/src/components/Listing.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import { ListGroup, Button, Modal, Col, Thumbnail, Grid, Row, Panel } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Chatroom from './Chatroom.jsx';
 
 var boxStyle = {
   boxShadow: '3px 3px 5px 6px grey',
@@ -159,19 +160,19 @@ class Listing extends React.Component {
         if (!this.state.completed) { //if not all parties have received the goods
             if (!this.state.arrived) { //if initializer has not yet notified the arrival of goods
                 if (!this.state.packed) { //if wolfpack is not yet filled
-                    footer = (<div>Your Wolfpack Is Asssembling...</div>);
+                    footer = (<div>Waiting on more buyers...</div>);
                 } else { //if wolfpack is filled
                     footer = (
                         <div>
-                            Wolfpack Assembled! Go get the goods!
-                            <Button onClick={this.handleArrive}>Goods are here!</Button>
+                            Product is ready to be picked up!
+                            <Button onClick={this.handleArrive}>Pick up product</Button>
                         </div>);
                 }
             } else { //if initializer has notified the arrival of goods
-                footer = (<div>The pack has been notified. They will come pick up soon...</div>);
+                footer = (<div>Fellow buyers have been notified and will be picking up the product soon!</div>);
             }
         } else { //if all parties have received the goods
-            footer = (<div>Mission Complete!</div>);
+            footer = (<div>Successful purchase</div>);
         }
       } else { //if current user is not the initializer for this listing
         var involved = this.state.listingParticipants.some(listing => {
@@ -181,18 +182,18 @@ class Listing extends React.Component {
           // this.hideJoinButton();
             if (!this.state.arrived) { //if initializer has not yet notified the arrivial of goods
                 if (!this.state.packed) { //if the pack is not filled
-                    footer = (<div>Waiting for the Rest of the Pack to Assemble</div>);
+                    footer = (<div>Waiting on more buyers...</div>);
                 } else { //if the pack is filled
-                    footer = (<div>Packed Assembled! Goods Will Arrive Soon!</div>);
+                    footer = (<div>Product is ready to be picked up!</div>);
                 }
             } else { //if initializer has notified the arrivial of goods
                 if (this.state.received) { //if current user has confirmed the receipt of goods
-                    footer = (<div>Thanks for being part of the Pack! Could not have done it without you!</div>);
+                    footer = (<div>You've picked up the product. Enjoy!</div>);
                 } else { //if current user has not confirmed the receipt of goods
                     footer = (
                         <div>
-                            Goods are here! Go pick it up from the Pack Leader!
-                            <Button onClick={this.handleReceive}>Received Goods!</Button>
+                            Product is ready to be picked up!
+                            <Button onClick={this.handleReceive}>Received</Button>
                         </div>);
                 }
             }
@@ -201,7 +202,7 @@ class Listing extends React.Component {
                 if (!this.state.packed) { //if this pack is not yet filled
                     // footer = (<Button onClick={this.handleJoin}>Join the Pack</Button>);
                 } else { //if this pack is already filled
-                    footer = (<div>Sorry, this Pack is full.</div>);
+                    footer = (<div>Sorry, this product has enough buyers.</div>);
                 }
             } else {
                 footer = (<div>Listing Closed</div>);
@@ -212,23 +213,35 @@ class Listing extends React.Component {
       return (
         <div>
 
-          <Modal show={this.state.lgShow}  bsSize="large" aria-labelledby="contained-modal-title-sm"  onKeyDown={this.handleKeyDown}>
+          <Modal show={this.state.lgShow} bsSize="large" aria-labelledby="contained-modal-title-sm" onKeyDown={this.handleKeyDown}>
             <Modal.Header >
               <Modal.Title id="contained-modal-title-sm"><div>{this.props.listingInfo.name}</div></Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              <ul>
-              <li>listing id: {this.props.listingInfo.id}</li>
-          		<li>listing name: {this.props.listingInfo.name}</li>
-          		<li>initializer: {this.props.listingInfo.initializer}</li>
-          		<li>price: {this.props.listingInfo.price}</li>
-          		<li>pick up location: {this.props.listingInfo.location}</li>
-          		<li>required num of wolves to join: {this.props.listingInfo.num_of_participants}</li>
-              <li>num of wolves joined: {this.state.listingParticipants.length}</li>
-              <li>num of wolves who received the goods: {this.state.receivedParticipants.length}</li>
-              <li>Description: {this.props.listingInfo.description}</li>
-              <li>Link: <a href={this.props.listingInfo.url} target="_blank">{this.props.listingInfo.name}</a></li>
-          	</ul>
+                <Grid>
+                    <Row>
+                        <Col md={5}>
+                          <ul>
+                              <li>Listing ID: {this.props.listingInfo.id}</li>
+                              <li>Description: {this.props.listingInfo.description}</li>
+                                <li>Purchaser: {this.props.listingInfo.initializer}</li>
+                                <li>Price: {this.props.listingInfo.price}</li>
+                                <li>Pickup Location: {this.props.listingInfo.location}</li>
+                                <li>Required Number of Buyers: {this.props.listingInfo.num_of_participants}</li>
+                              <li>Number of Buyers Joined: {this.state.listingParticipants.length}</li>
+                              <li>Number of Buyers Who Have Picked Up Product: {this.state.receivedParticipants.length}</li>
+                              <li>Buy Here: <a href={this.props.listingInfo.url} target="_blank">{this.props.listingInfo.name}</a></li>
+                      	 </ul>
+                        </Col>
+                        <Col md={7}>
+                            <Chatroom userId={this.props.userId} username={this.props.username} listingInfo={this.state.listing} listingId={this.state.listingId} socket={this.props.socket}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <div>MAP COMPONENT</div>
+                    </Row>
+                </Grid>
+
 
             </Modal.Body>
             <Modal.Footer>
