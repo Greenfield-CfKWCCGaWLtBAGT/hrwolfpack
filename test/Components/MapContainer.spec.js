73de15e4050ff738ab92dd8644fdb80feb28a781
tyler carson <tyler.c.carson@gmail.com>
2017-08-25 23:40:19 -0700

    client tests

--- test/Components/MapContainer.spec.js
+++ test/Components/MapContainer.spec.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import MapContainer from '../../client/src/components/MapContainer.jsx';
-import Map from '../../client/src/components/Map.jsx';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import {Grid, Row, Col} from 'react-bootstrap';
 import { expect } from 'chai';
-import chaiEnzyme from 'chai-enzyme';
 import { mount, shallow } from 'enzyme';
-import sinon from 'sinon';
-
-//chai.use(chaiEnzyme());
 
 describe('<MapContainer /> component', () => {
 	it('has style for height position and style', () => {
-		//not rendering here... what do I need as a dependency?
 		const wrapper = shallow(<MapContainer />);
 		expect(wrapper).to.have.style('position', 'absolute');
 		expect(wrapper).to.have.style('height', '100%');
--- test/Server_Database/mock-db.js
+++ test/Server_Database/mock-db.js
@@ -59,3 +59,35 @@ module.exports.listings = [
 		num_of_participants: 4
 	}
 ]
+
+module.exports.deals = [
+	{	
+		description: "Sleep Lavender Bath Salts 2lbs Luxury Bag",
+		id:	"B00AYNNX34",
+		large_image: "https://images-na.ssl-images-amazon.com/images/I/51gKoCrgyyL.jpg",
+		list_price:	"14.99",
+		long_description:	"PACIFIC OCEAN SEA SALTS:",
+		medium_image:	"https://images-na.ssl-images-amazon.com/images/I/51gKoCrgyyL._SL160_.jpg",
+		small_image:	"https://images-na.ssl-images-amazon.com/images/I/51gKoCrgyyL._SL75_.jpg",
+		url:	"https://www.amazon.com/Sleep-Lavender-Bath-Salts-Luxury/dp/B00AYNNX34"
+	},
+	{
+		description: "Dr. Teals Epsom Salt Soaking Solution Bundle - 1 Relax & Relief Eucalyptus Spearmint 3lbs and 1 Sooth & Sleep Lavender 3lbs",
+		id:	"B00HMSU4ZE",
+		large_image: "https://images-na.ssl-images-amazon.com/images/I/61hPbsWjlML.jpg",
+		list_price:	"17.31",
+		long_description:	"Dr. Teals Epsom Salt Soaking Solution Bundle - 1 Relax & Relief Eucalyptus Spearmint 3lbs and 1 Sooth & Sleep Lavender 3lbs",
+		medium_image:	"https://images-na.ssl-images-amazon.com/images/I/61hPbsWjlML._SL160_.jpg",
+		small_image:	"https://images-na.ssl-images-amazon.com/images/I/61hPbsWjlML._SL75_.jpg",
+		url:	"https://www.amazon.com/Dr-Teals-Soaking-Solution-Bundle/dp/B00HMSU4ZE"
+	},
+	{
+		description: "Dead Sea Mineral Bath Salt Variety 3 Pack: Pure Dead Sea Salt, Lavender Dead Sea Salt and Eucalyptus Dead Sea Salt (1.75lb bag of each)",
+		id:	"B00BPF9CYE",
+		large_image: "https://images-na.ssl-images-amazon.com/images/I/51r%2BSxoLnqL.jpg",
+		list_price:	"39.99",
+		long_description:	"DEAD SEA VARIETY 3 PACK OF 1.75 LB BAGS INCLUDES: ",
+		small_image:	"https://images-na.ssl-images-amazon.com/images/I/51r%2BSxoLnqL._SL75_.jpg",
+		url:	"https://www.amazon.com/Dead-Mineral-Bath-Salt-Variety/dp/B00BPF9CYE"
+	}
+]
